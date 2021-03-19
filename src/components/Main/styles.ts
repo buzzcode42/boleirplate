8114aaf280f48a75c2ed3bc94be886d6a15d987f
Fernando dos Santos <nando4803@gmail.com>
2021-03-19 15:02:36 +0100

    add line-height

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -19,11 +19,13 @@ export const Logo = styled.img`
 `
 
 export const Title = styled.h1`
-  font-size: 2.5rem;
+  font-size: 3rem;
+  line-height: 2rem;
 `
 
 export const Description = styled.h2`
-  font-size: 2rem;
+  font-size: 2.5rem;
+  line-height: 1.5rem;
   font-weight: 400;
 `
 
